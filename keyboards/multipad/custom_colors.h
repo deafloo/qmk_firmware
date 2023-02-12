ee628c8a16da3fb6b74c2e32ac41f2e29f673c6e
deafloo <florian.denkmeier@fau.de>
2023-02-12 22:35:44 +0100

    Add per key coloring.

---
+++ keyboards/multipad/custom_colors.h
@@ -0,0 +1,14 @@
+# define HSV_blue7 240, 255, 220   //hsl(240,100,86)
+# define HSV_blue8 240, 255, 240   //hsl(240,100,94)
+# define HSV_blue9 240, 255, 255   //hsl(240,100,100)
+# define HSV_blue4 240, 255, 160   //hsl(240,100,63)
+# define HSV_blue5 240, 255, 180   //hsl(240,100,71)
+# define HSV_blue6 240, 255, 200   //hsl(240,100,78)
+# define HSV_blue1 240, 255, 100   //hsl(240,100,39)
+# define HSV_blue2 240, 255, 120   //hsl(240,100,47)
+# define HSV_blue3 240, 255, 140   //hsl(240,100,55)
+# define HSV_blue0 240, 255, 100
+
+# define HSV_custom_green 120, 255, 199
+# define HSV_function_yellow 63, 255, 234
+# define HSV_custom_red 2, 255, 234
--- keyboards/multipad/keymaps/deafloo/keymap.c
+++ keyboards/multipad/keymaps/deafloo/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 #include <keymap_german.h>
+#include <color.h>
+#include "custom_colors.h"
 // Helpful defines
 //#define GRAVE_MODS  (MOD_BIT(KC_LSFT)|MOD_BIT(KC_RSFT)|MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI)|MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))
 
@@ -17,7 +19,9 @@
 enum layer_names{
     NUMPAD,
     MATH,
-    MAKRO
+    ARROWS,
+    MAKRO,
+    GAMING
 };
 
 // Switch behavior
@@ -50,23 +54,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUMPAD] = LAYOUT_numpad_5x4(
         RGB_VAI, RGB_TOG, KC_CALC,                  \
             KC_P7,    KC_P8,    KC_P9,              \
-            KC_P4,    KC_P5,    KC_P6,    KC_TAB,   \
-            KC_P1,    KC_P2,    KC_P3,    TO(MATH),    \
+            KC_P4,    KC_P5,    KC_P6,    TO(MATH),   \
+            KC_P1,    KC_P2,    KC_P3,    TO(NUMPAD),    \
             KC_P0,    KC_DOT,  KC_PDOT,  KC_PENT
     ),
     [MATH] = LAYOUT_numpad_5x4(
         _______, _______, _______,
             _______,    _______,    _______,
-            _______,    _______,    _______,    _______,
-            _______,    _______,    _______,    TO(MAKRO),    
+            _______,    _______,    _______,    TO(ARROWS),
+            _______,    _______,    _______,    _______,    
             _______,    _______,    _______,    _______
+    ),
+    [ARROWS] = LAYOUT_numpad_5x4(
+        _______, _______, _______,
+            KC_BSPC,    KC_DEL,    KC_HOME,
+            KC_TAB,     KC_UP,     KC_END,      TO(GAMING),
+            KC_LEFT,    KC_DOWN,   KC_RIGHT,    _______,
+            KC_SPACE,   _______,   _______,     KC_ENT
     ),
     [MAKRO] = LAYOUT_numpad_5x4(
         _______, _______, _______,
             _______,    _______,    _______,
-            _______,    _______,    _______,    _______,
-            _______,    _______,    _______,    TO(NUMPAD),    
+            _______,    _______,    _______,    TO(GAMING),
+            _______,    _______,    _______,    _______,    
             _______,    _______,    _______,    _______
+    ),
+    [GAMING] = LAYOUT_numpad_5x4(
+        _______, _______, KC_ESC,
+            KC_TAB,     KC_I,       KC_M,
+            KC_Q,       KC_W,       KC_E,       TO(NUMPAD),
+            KC_A,       KC_S,       KC_D,       KC_G,
+            KC_LSFT,    KC_LCTL,    KC_C,       KC_SPACE
+
     )
 };
 
@@ -75,10 +94,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 led_config_t g_led_config = { 
 {   // Key Matrix to LED index
     {NO_LED, NO_LED, NO_LED},
-    {0,  1,  2},
-    {3,  4,  5,  6},
-    {NO_LED, 7,  8,  9},
-    {10, 11, 12, 13}
+        {13,   12,  11},
+        {10,   9,   8,   7},
+{NO_LED, 6,    5,   4},
+        {3,    2,   1,   0}
 },
 {
     // LED index to Physical Position
@@ -90,12 +109,87 @@ led_config_t g_led_config = {
 {
     // LED index to Flag
     4,4,4,
-    4,4,4,1,
+    4,4,4,4,
     4,4,4,
-    4,1,4,4
+    4,4,4,4
 }
 };
 
+
+extern rgb_config_t rgb_matrix_config;
+
+void keyboard_post_init_user(void){
+    rgb_matrix_enable();
+}
+
+const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
+    [NUMPAD] =  { {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green}, {HSV_blue0},
+                  {HSV_blue3}, {HSV_blue2}, {HSV_blue1},
+                  {HSV_function_yellow}, {HSV_blue6}, {HSV_blue5}, {HSV_blue4},
+                  {HSV_blue9}, {HSV_blue8}, {HSV_blue7} },
+
+    [MATH] =    { {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green},
+                  {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green},
+                  {HSV_function_yellow}, {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green},
+                  {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green} },
+
+    [ARROWS] =  { {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green}, 
+                  {HSV_blue5}, {HSV_blue5}, {HSV_blue5}, 
+                  {HSV_function_yellow}, {HSV_custom_green}, {HSV_blue5}, {HSV_custom_green}, 
+                  {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green} },
+
+    [MAKRO] =   { {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green},
+                  {HSV_PINK}, {HSV_TURQUOISE}, {HSV_RED},
+                  {HSV_function_yellow}, {HSV_TEAL}, {HSV_MAGENTA}, {HSV_ORANGE},
+                  {HSV_AZURE}, {HSV_BLUE}, {HSV_GREEN} },
+
+    [GAMING] =  { {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green}, 
+                  {HSV_custom_red}, {HSV_custom_red}, {HSV_custom_red}, 
+                  {HSV_function_yellow}, {HSV_custom_green}, {HSV_custom_red}, {HSV_custom_green}, 
+                  {HSV_custom_green}, {HSV_custom_green}, {HSV_custom_green} }
+};
+
+void set_layer_color(int layer) {
+  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
+    HSV hsv = {
+      .h = pgm_read_byte(&ledmap[layer][i][0]),
+      .s = pgm_read_byte(&ledmap[layer][i][1]),
+      .v = pgm_read_byte(&ledmap[layer][i][2]),
+    };
+    if (!hsv.h && !hsv.s && !hsv.v) {
+        rgb_matrix_set_color( i, 0, 0, 0 );
+    } else {
+        RGB rgb = hsv_to_rgb( hsv );
+        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+    }
+  }
+}
+
+void matrix_scan_user(void){
+  if (!rgblight_is_enabled()) { return; }
+  switch (biton32(layer_state)) {
+    case NUMPAD:
+      set_layer_color(NUMPAD);
+      break;
+    case MATH:
+      set_layer_color(MATH);
+      break;
+    case ARROWS:
+      set_layer_color(ARROWS);
+      break;
+    case MAKRO:
+      set_layer_color(MAKRO);
+      break;
+    case GAMING:
+      set_layer_color(GAMING);
+      break;
+   default:
+    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+      rgb_matrix_set_color_all(0, 0, 0);
+    break;
+  }
+}
 
 // Encoder behavior
 bool encoder_update_user(uint8_t index, bool clockwise) {
@@ -108,6 +202,12 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             break;
         case MAKRO:
             clockwise ? rgblight_increase_hue():rgblight_decrease_hue();
+            break;
+        case ARROWS:
+            clockwise ? tap_code(KC_WH_U):tap_code(KC_WH_D);
+            break;
+        case GAMING:
+            clockwise ? tap_code(KC_VOLU):tap_code(KC_VOLD);
             break;
     }
     return false;
@@ -131,6 +231,12 @@ bool oled_task_user(void) {
             break;
         case MAKRO:
             oled_write_P(PSTR("Makro\n"), false);
+            break;
+        case ARROWS:
+            oled_write_P(PSTR("Movement\n"), false);
+            break;
+        case GAMING:
+            oled_write_P(PSTR("Gaming\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
